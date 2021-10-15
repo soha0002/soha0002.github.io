fa1ed36583d97ed07fb65b48f470595f9f6d0086
soha0002 <sophieohara00@gmail.com>
2021-10-15 12:51:26 +1100

    update

--- 6.chrome/assets/js/main.js
+++ 6.chrome/assets/js/main.js
@@ -1,11 +1,15 @@
 // Start marquee
-$('.marquee_text').marquee({
-    direction: 'left',
-    duration: 50000,
-    gap: 50,
-    delayBeforeStart: 0,
-    duplicated: true,
-    startVisible: true
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $('.marquee_text').marquee({
+        direction: 'left',
+        duration: 50000,
+        gap: 50,
+        delayBeforeStart: 0,
+        duplicated: true,
+        startVisible: true
+    });
 });
 
 
