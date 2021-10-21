aa4e504300e2a9cbd25e96d19ac7d671023db003
soha0002 <sophieohara00@gmail.com>
2021-10-21 22:33:05 +1100

    week12 update

--- 6.chrome/assets/js/main.js
+++ 6.chrome/assets/js/main.js
@@ -4,7 +4,15 @@
 $( document ).ready(function() {
     $('.marquee_text').marquee({
         direction: 'left',
-        duration: 50000,
+        duration: 100000,
+        gap: 50,
+        delayBeforeStart: 0,
+        duplicated: true,
+        startVisible: true
+    });
+    $('.marquee_text2').marquee({
+        direction: 'leftright',
+        duration: 100000,
         gap: 50,
         delayBeforeStart: 0,
         duplicated: true,
